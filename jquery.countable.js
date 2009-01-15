9fc7eedbb6baf31e3f43031cde2a4d5f1e0767df
brandonaaron <brandon.aaron@gmail.com>
2009-01-15 13:03:07 -0600

    stop repeating the same check over and over again

--- jquery.countable.js
+++ jquery.countable.js
@@ -8,7 +8,7 @@
 $.fn.extend({
 	countable: function(options) {
 		return this.each(function() {
-			var $this = $(this), interval, $el;
+			var $this = $(this), interval, prev_char_diff, $el;
 			options = $.extend({
 				threshold: 0.5,
 				appendMethod: 'insertAfter', // insertBefore || insertAfter
@@ -22,20 +22,30 @@ $.fn.extend({
 				negativeCopy: "You are {n} characters over.",
 				fadeDuration: 'normal'
 			}, options);
+			
 			$el = $('<'+options.tagName+'/>')
 				.html( options.positiveCopy.replace("{n}", '<span class="num"/>') )
 				.addClass( options.className )
 				.css({ opacity: 0 });
 			$el[options.appendMethod]($this);
+			
 			$this
 				.bind('keyup', check)
 				.bind('focus blur', function(event) {
 					if ( event.type == 'blur' ) clearInterval( interval );
 					if ( event.type == 'focus' && !interval ) setInterval(check, options.interval);
 				});
+			
 			function check() {
 				var val = $this.val(), length = val.length, percentage_complete = length/options.maxLength, char_diff = options.maxLength - length;
+				if ( prev_char_diff != undefined && char_diff == prev_char_diff ) return;
 				opacity = options.startOpacity + ((options.threshold - percentage_complete) * ((options.startOpacity * 2) - 2));
+				
+				if ( $el.is(':hidden') && percentage_complete >= options.threshold )
+					$el.show();
+				if ( $el.is(':visible') && percentage_complete < options.threshold )
+					$el.hide();
+					
 				$el.stop().fadeTo( options.fadeDuration, percentage_complete >= options.threshold ? opacity : 0 );
 				if ( char_diff >= 0 ) {
 					if ( $el.is( '.'+options.maxClassName ) )
@@ -44,12 +54,16 @@ $.fn.extend({
 					if ( !$el.is( '.'+options.maxClassName ) )
 						$el.html( options.negativeCopy.replace("{n}", '<span class="num"/>') );
 				}
+				
 				$el[ (char_diff < 0 ? 'add' : 'remove') + 'Class' ]( options.maxClassName );
 				$el.find('.num').text( Math.abs(char_diff) );
+				
 				if ( char_diff == -1 || char_diff == 1 )
 					$el.html( $el.html().replace(/characters\b/, "character") );
 				else
 					$el.html( $el.html().replace(/character\b/, "characters") );
+					
+				prev_char_diff = char_diff;
 			};
 			check();
 		});
